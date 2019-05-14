deb6ef73ea1d2b7c92a27bc39a069e004fc6d934
Cipuzp <zhupan97njy@gmail.com>
2019-05-14 23:17:16 +0800

    first_commit

---
+++ src/components/bubble/bubble.vue
@@ -0,0 +1,33 @@
+<template>
+  <span class="bubble">
+    {{ num }}
+  </span>
+</template>
+
+<script>
+  export default {
+    name: 'bubble',
+    props: {
+      num: {
+        type: Number
+      }
+    }
+  }
+</script>
+
+<style lang="stylus" scoped>
+  @import "~common/stylus/variable"
+
+  .bubble
+    display: inline-block
+    padding: 0 5px
+    height: 15px
+    line-height: 18px
+    text-align: center
+    border-radius: 15px
+    font-family: Helvetica
+    font-weight: 700
+    font-size: $fontsize-small-s
+    color: $color-white
+    background: linear-gradient(to right, $color-orange, $color-red)
+</style>
